41378e07c5fb78d528e6ecb27b2515d202d4bf82
Yazhini Venkatesan <156333528+YazhiniVenkatesan12@users.noreply.github.com>
2024-09-24 23:01:42 +0530

    Delete ex3 directory

--- ex3/models/Post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PostSchema = new mongoose.Schema({
-    content: { type: String, required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-});
-
-module.exports = mongoose.model('Post', PostSchema);
